59758c367fc2f7e36f1cbe2d479467e236a0e567
Lee Jia Keat <leejiakeat@gmail.com>
2023-08-26 14:35:11 +0800

    shit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  constructor(private http: HttpClient) { }
+
+  magic(){
+    this.http.get('http://localhost:8080/').subscribe(data => {
+      console.log(data);
+    });
+  }
+}
