221f488f658dd86bdea99833bc3a5efc69ae4d37
Wilfred (Accure) <wnjh98@gmail.com>
2023-08-26 13:52:25 +0800

    small edits to gitignore and refinement

--- text_to_speech/textspeech.py
+++ text_to_speech/textspeech.py
@@ -1,7 +1,33 @@
+'''*****************************************************
+pyttsx3 (offline TTS for Python 3)
+*****************************************************'''
 import pyttsx3 
 
 # https://stackoverflow.com/questions/76434535/attributeerror-super-object-has-no-attribute-init
-engine = pyttsx3.init()
-engine.say("Deon is such a god. I'm so in love with him, i'm wet")
-engine.runAndWait()
+engine = pyttsx3.init() # object creation
 
+# """ RATE"""
+# rate = engine.getProperty('rate')   # getting details of current speaking rate
+# print (rate)                        #printing current voice rate
+# engine.setProperty('rate', 125)     # setting up new voice rate
+
+
+# """VOLUME"""
+# volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
+# print (volume)                          #printing current volume level
+# engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
+
+"""VOICE"""
+# voices = engine.getProperty('voices')       #getting details of current voice
+# #engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+# engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
+
+engine.say("Hello World!")
+# engine.say('My current speaking rate is ' + str(rate))
+engine.runAndWait()
+engine.stop()
+
+"""Saving Voice to a file"""
+# On linux make sure that 'espeak' and 'ffmpeg' are installed
+# engine.save_to_file('Hello World', 'test.mp3')
+engine.runAndWait()
