b672c5d8b9563571ae6af32eaa6f52cef604ba2a
Lee Jia Keat <leejiakeat@gmail.com>
2023-08-26 21:27:21 +0800

    test

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,1 +1,4 @@
-export const API_URL : string = "https://seesay.azurewebsites.net/";
+export const PRODUCTION_API_URL = "https://seesay.azurewebsites.net/";
+export const DEV_API_URL : string = "http://localhost:5000/";
+export const IS_DEV : boolean = false;
+export const API_URL : string = IS_DEV ? DEV_API_URL : PRODUCTION_API_URL;
