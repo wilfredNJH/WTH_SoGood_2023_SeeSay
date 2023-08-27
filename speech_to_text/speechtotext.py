826e841b7a295914658ac5c59fb45422796d81fa
Deon Khong <72732410+deonkhong@users.noreply.github.com>
2023-08-27 09:43:04 +0800

    test

--- speech_to_text/speechtotext.py
+++ speech_to_text/speechtotext.py
@@ -3,6 +3,16 @@ import openai
 import constants
 
 openai.api_key = constants.OPENAI_API_KEY
-f = open(os.path.dirname(__file__) + "\\recording0.wav", "rb")
+
+# Function definition
+def save_transcript_to_file(transcript, filename):
+    with open(filename, 'w') as file:
+        file.write(transcript)
+
+
+file_path = os.path.join(os.path.dirname(__file__), "recording0.wav")
+f = open(file_path, "rb")
 transcript = openai.Audio.transcribe("whisper-1", f, language="en")
 print(transcript)
+# Save the transcript to a .txt file
+save_transcript_to_file(transcript['text'], os.path.join(os.path.dirname(__file__), "transcript0.txt"))
